c7116dd6625babf16e3a599d6e49a43d3b1138bc
Bannasorn Sukan <136978678+Bannasorn@users.noreply.github.com>
2023-08-16 23:52:32 +0700

    hw 5-2

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -24,32 +24,28 @@
 #include <stdio.h>
 int main()
 {
-    char string1 [50], string2 [50], string3 [50], string4 [50], s4[50] ;
-    int a = 0, b = 1, c = 2, d = 3, e = 4 ;
-    printf("Please Enter your string : ");
-    scanf("%s %s %s %s", string1,string2,string3,string4);
-    if (e > d)
-    {
-        printf("Result: %s", string4);
-        if (e > c)
-        {
-            printf(" %s", string3);
-            if (e > b)
-            {
-                printf(" %s", string2);
-                if (e > a)
-                {
-                    printf(" %s", string1);
-                }
-                
-            }
-            
-        }
-        
-    }
-    else{
-        printf("Error");
-    }
+    int a = 0, b = 0, c = 0 ;
+    printf("Input[1] ") ; scanf( "%d", &a ) ;
+    printf("Input[2] ") ; scanf( "%d", &b ) ;
+    printf("Input[3] ") ; scanf( "%d", &c ) ;
     
+    if( a > b && a > c ) {
+        printf( "%d ", a ) ;
+        if( b > c ) printf( "%d %d ", b,c ) ;
+        else printf( "%d %d ", c,b ) ;
+    }
+
+    else if( b > a && b > c ) {
+        printf( "%d ", b ) ;
+        if( a > c ) printf( "%d %d ", a, c ) ;
+        else printf( "%d %d ", c,a ) ;
+    }
+
+    else if( c > b && c > a ) {
+        printf( "%d ", c ) ;
+        if( b > a ) printf( "%d %d ", b,a ) ;
+        else printf( "%d %d ", a,b ) ;
+    }
+
     return 0;
 }
